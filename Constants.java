eb8e0430d367206af3eaa3661862a7c28133a4aa
Himanshu <guptahimanshu479@gmail.com>
2019-12-08 02:20:47 +0530

    Adding constants

---
+++ Constants.java
@@ -0,0 +1,13 @@
+package sudoku;
+
+public class Constants {
+
+	private Constants(){
+		
+	}
+	
+	public static final int BOARD_SIZE = 9;
+	public static final int MIN_NUMBER = 1;
+	public static final int MAX_NUMBER = 9;
+	public static final int BOX_SIZE = 3;
+}
